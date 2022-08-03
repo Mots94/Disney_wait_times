5f1141a3f8660dbf44397dda79f1f2144215f256
Mots94 <rmotykow@gmail.com>
2022-08-03 18:13:07 -0500

    developing flask route

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 # Include dependencies
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 # import scraping
 import os
 
 # setup Flas
 app = Flask(__name__)
 
+connection_string = f"mongodb+srv://purpleteam:{os.environ.get('MONGO_PASSWORD', 'password')}@disney-data-cluster.guziap7.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+db = client.disneyData
+collection = db.waitTimes
+
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = f"mongodb+srv://purpleteam:{os.environ.get('MONGO_PASSWORD', 'password')}@disney-data-cluster.guziap7.mongodb.net/?retryWrites=true&w=majority"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = f"mongodb+srv://purpleteam:{os.environ.get('MONGO_PASSWORD', 'password')}@disney-data-cluster.guziap7.mongodb.net/?retryWrites=true&w=majority"
+# mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    disneydata = mongo.db.disneyData.rideTimes.find_one()
-    return render_template("index.html", disneydata=disneydata)
+    return render_template("index.html")
 
 # @app.route("/scrape")
 # def scrape():
@@ -25,4 +30,4 @@ def index():
 #     return redirect('/',code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(port=8000)
