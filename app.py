6f04de095c611143940132807371e13cfdf1fd3d
Mots94 <rmotykow@gmail.com>
2022-08-02 19:39:33 -0500

    updating flask app with db connection

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 # Include dependencies
-from curses import meta
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+# import scraping
+import os
 
 # setup Flas
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "xyz"
+app.config["MONGO_URI"] = f"mongodb+srv://purpleteam:{os.environ.get('MONGO_PASSWORD', 'password')}@disney-data-cluster.guziap7.mongodb.net/?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    disneymeta = mongo.db.disney-final-project-database.find_one()
+    disneymeta = mongo.db.disneyData.rideTimes.find_one()
     return render_template("index.html",meta=disneymeta)
-    disneydata = mongo.db.disney-final-project-database.find_one()
-    return render_template("index.html",data=disneydata)
 
 @app.route("/scrape")
 def scrape():
-    data=mongo.db.disney_data
-    disney_data = scraping.scrape_all()
-    disney.update_one({},{"$set":disney_data},upsert=True)
+    data=mongo.db.disneyData.rideTimes
+    # disney_data = scraping.scrape_all()
+    disney_data = {"name": "Magic Kingdom", "ride": "Tropic Thunder"}
+    data.update_one({},{"$set":disney_data},upsert=True)
     return redirect('/',code=302)
 
 if __name__ == "__main__":
