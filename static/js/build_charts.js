f6aa3473023b19f964961358dc43c634f1525e78
Mots94 <rmotykow@gmail.com>
2022-08-14 14:11:32 -0500

    added sticky class to date selector

--- static/js/build_charts.js
+++ static/js/build_charts.js
@@ -171,7 +171,7 @@ function buildCharts(date) {
     
 };
   
-//   // Create a Ride Bubble Chart
+  // Create a Ride Bubble Chart
   
 //       // Create the trace for the bubble chart.
 //       var bubbleData = [
@@ -202,52 +202,7 @@ function buildCharts(date) {
   
 //       // 3. Use Plotly to plot the data with the layout.
 //       Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-  
-  
-//   // Create Temperature Gauge Chart
-//   // 1. Create a variable that filters the metadata array for the object with the desired date.
-//   var metadata = data.metadata;
-  
-//   // 2. Create a variable that holds the first sample in the metadata array.
-//   var gaugeArray2 = metadata.filter(metadataObj => metadataObj.id == sample);
-//   var gaugeResult2 = gaugeArray2[0];
-  
-//   // 3. Create a variable that holds the washing frequency.
-//   var tempGauge = gaugeResult2.wfreq;
-  
-//   // 4. Create the trace for the gauge chart.
-//   var gaugeData2 = [
-//   {
-//     title: { text: "<b>Average Temperature</b>" },
-//     value: parseFloat(tempGauge),
-//     type: "indicator",
-//     mode: "gauge+number",
-//     gauge: {
-//       axis: { range: [40, 90], tickwidth: 10, tickcolor: "black" },
-//       bar: { color: "black" },
-//       steps: [
-//         { range: [40, 50], color: "green" },
-//         { range: [50, 60], color: "lightgreen" },
-//         { range: [60, 70], color: "yellow" },
-//         { range: [70, 80], color: "orange" },
-//         { range: [80, 90], color: "red" },
-//       ],
-//     }
-//   }
-//   ];
-  
-//   // 5. Create the layout for the gauge chart.
-//   var gaugeLayout = { 
-//          width: 500, 
-//          height: 425, 
-//          margin: { t: 0, b: 0 } };
-  
-//   // 6. Use Plotly to plot the gauge data and layout.
-//   Plotly.newPlot("temp_gauge", gaugeData2, gaugeLayout);
-  
-  
-//     });
-//   }
+
 
   // Info. Panel 
   // function buildMetadata() {
